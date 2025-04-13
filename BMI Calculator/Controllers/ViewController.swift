102f2277cc0ba86c344fe214ad560341dab89b61
nibim <nibm@kth.se>
2025-04-13 18:33:01 +0200

    Feature: the sliders connected as an action as well the units were assigned to the height and weight

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -4,17 +4,27 @@
 //
 //  Created by Angela Yu on 21/08/2019.
 //  Copyright © 2019 Angela Yu. All rights reserved.
-//
+
 
 import UIKit
 
 class ViewController: UIViewController {
 
+
+    @IBOutlet weak var heightLable: UILabel!
+    @IBOutlet weak var weightLable: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // Do any dditional setup after loading the view.
     }
 
-
+    @IBAction func heightSliderChanged(_ sender: UISlider) {
+        heightLable.text = "\(String(format:"%.2f", sender.value))m"
+    }
+    
+    @IBAction func weightSliderChanged(_ sender: UISlider) {
+        weightLable.text = "\(String(format:"%.2f", sender.value))Kg"
+    }
 }
 
