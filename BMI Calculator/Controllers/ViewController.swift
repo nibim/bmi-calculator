3daa3996b6683a527cdecbf6feca3915b94d7c4b
nibim <nibm@kth.se>
2025-04-14 13:11:05 +0200

    Feature: the calculatedPressed is built and it opens the secondViewController, showing calculated bmi

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -14,6 +14,9 @@ class ViewController: UIViewController {
     @IBOutlet weak var heightLable: UILabel!
     @IBOutlet weak var weightLable: UILabel!
     
+    @IBOutlet weak var heightSlider: UISlider!
+    @IBOutlet weak var weightSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any dditional setup after loading the view.
@@ -25,6 +28,15 @@ class ViewController: UIViewController {
     
     @IBAction func weightSliderChanged(_ sender: UISlider) {
         weightLable.text = "\(String(format:"%.2f", sender.value))Kg"
+    }
+    @IBAction func calculatedPressed(_ sender: UIButton) {
+        let height =  heightSlider.value
+        let weight =  weightSlider.value
+        let bmi = weight / (height * height)
+        let secondVC = secondViewController()  //make an object from the secondView
+        secondVC.bmiValue = String(format:"%.1f", bmi)
+        self.present(secondVC, animated: true, completion: nil)     //secondView is represented
+        
     }
 }
 
---
+++ BMI Calculator/Controllers/secondViewController.swift
@@ -0,0 +1,9 @@
+//
+//  secondViewController.swift
+//  BMI Calculator
+//
+//  Created by Nima Beig Mohammadi on 14.04.25.
+//  Copyright © 2025 Angela Yu. All rights reserved.
+//
+
+import Foundation
